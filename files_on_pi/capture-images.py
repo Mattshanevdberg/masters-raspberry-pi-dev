5a33d31f70e5cbc895127e60d86542b76ecc1b88
Matthew Van den Berg <msvdb3@gmail.com>
2023-09-14 08:40:02 +0200

    added code for using the new camera, added a variable for user_name of pi

---
+++ files_on_pi/capture-images.py
@@ -0,0 +1,35 @@
+import os
+import time
+import picamera
+
+def capture_images(user_name, output_folder, num_images, interval_seconds):
+    # Create the output folder on the desktop if it doesn't exist
+    desktop_path = f'/home/{user_name}/Desktop/{output_folder}'
+    if not os.path.exists(desktop_path):
+        os.makedirs(desktop_path)
+    
+    # Initialize the camera
+    with picamera.PiCamera() as camera:
+        for i in range(num_images):
+            # Get the current time for naming the image
+            current_time = time.strftime('%Y-%m-%d_%H-%M')
+            
+            # Capture an image
+            image_path = os.path.join(desktop_path, f'image_{current_time}_{i + 1:03d}.jpg')
+            camera.capture(image_path)
+            
+            print(f'Captured image: {image_path}')
+            
+            # Wait for the specified interval
+            time.sleep(interval_seconds)
+
+if __name__ == "__main__":
+    output_folder = time.strftime('%Y-%m-%d_%H-%M')
+    num_images = 4
+    interval_seconds = 1
+    user_name = 'mattvdb'
+    
+    print(f'Capturing {num_images} images at {interval_seconds}-second intervals...')
+    capture_images(output_folder, num_images, interval_seconds)
+    print('Image capture complete.')
+
