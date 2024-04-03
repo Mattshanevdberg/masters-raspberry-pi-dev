4daef2cb733f1af615c34c4027b6d2a550337166
Mattshanevdberg <110899554+Mattshanevdberg@users.noreply.github.com>
2024-04-03 11:10:17 +0200

    Update test.py with camera checks
    Saving work on test.py

--- files_on_pi/OOP/test.py
+++ files_on_pi/OOP/test.py
@@ -13,6 +13,7 @@ print(time.time())
 time_.sleep(output)
 print(time.tiem())
 '''
+'''
 import drive
 from owntime import Timer
 access_token_timer = Timer('matthew')
@@ -26,3 +27,19 @@ drive_upload = drive.DriveUpload('matthew', drive_auth, access_token_timer)
 #drive_upload.upload_folders_to_drive()
 #drive.request_device_authorization()
 print(drive_upload.check_for_low_memory())
+'''
+import picamera2
+import os
+from picamera2.encoders import H264Encoder
+from picamera2.outputs import FfmpegOutput
+from pprint import *
+
+#initiate camera
+cam = picamera2.Picamera2()
+
+#check the available sensors and resolutions
+pprint(cam.sensor_modes)
+
+#video_config = video_config = picam2.create_video_configuration(main={"size": (1280, 720), "format": "RGB888"},
+                                                 lores={"size": lsize, "format": "YUV420"})
+
