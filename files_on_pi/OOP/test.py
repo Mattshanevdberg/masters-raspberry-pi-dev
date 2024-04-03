9c715113a4fbc403a1fa4f966a6c2afc9207dfd5
Mattshanevdberg <110899554+Mattshanevdberg@users.noreply.github.com>
2024-04-03 16:09:59 +0200

    Update test.py

--- files_on_pi/OOP/test.py
+++ files_on_pi/OOP/test.py
@@ -29,10 +29,11 @@ drive_upload = drive.DriveUpload('matthew', drive_auth, access_token_timer)
 print(drive_upload.check_for_low_memory())
 '''
 import picamera2
-import os
 from picamera2.encoders import H264Encoder
 from picamera2.outputs import FfmpegOutput
 from pprint import *
+import time
+from picamera2.outputs import FileOutput 
 
 #initiate camera
 cam = picamera2.Picamera2()
@@ -40,6 +41,26 @@ cam = picamera2.Picamera2()
 #check the available sensors and resolutions
 pprint(cam.sensor_modes)
 
-#video_config = video_config = picam2.create_video_configuration(main={"size": (1280, 720), "format": "RGB888"},
-                                                 lores={"size": lsize, "format": "YUV420"})
+#video_config = cam.create_video_configuration(main={"size": (4608, 2592), "format": "RGB888"}, buffer_count=2, controls={"FrameRate": 12})
+#video_config = cam.create_video_configuration(main={"size": (1280, 720), "format": "RGB888"}, buffer_count=2, controls={"FrameRate": 12})
+#video_config = cam.create_video_configuration(main={"size": (3840, 2160), "format": "RGB888"}, buffer_count=2, controls={"FrameRate": 12})
+video_config = cam.create_video_configuration(main={"size": (2560, 1440), "format": "RGB888"}, buffer_count=2, controls={"FrameRate": 12})
 
+cam.configure(video_config)
+
+#encoder = H264Encoder()
+encoder = JpegEncoder(q=80)
+
+#output = FfmpegOutput('test.mp4')
+
+cam.start() # potentially check that the camera started (a trouble shooting option)
+
+#encoder.output = FileOutput('test.mp4')
+
+encoder.output = FileOutput(f"{int(time.time())}.mjpeg")
+
+cam.start_encoder(encoder)
+
+time.sleep(2)
+
+cam.stop_encoder()
